0050ebe1460ad1ffd8009211e96d3cdff316a677
Willis Li <li.willis95@gmail.com>
2022-05-06 14:50:37 -0500

    Routes & Data

--- team/src/App.js
+++ team/src/App.js
@@ -1,11 +1,25 @@
 import './index.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import team from './data/team.json';
 
 function App() {
   return (
     <div className = "App">
-      <header className = "App-header">
+      <header className = "">
         <h1 className = "">Ctrl Alt Delete</h1>
+        <div className = "flex flex-row">
+          {team.map((member) => (
+            <NavLink className = "bg-slate-600 p-2" to = {`/${member.name}`}>
+              {member.name}
+            </NavLink>
+          ))}
+        </div>
       </header>
+      <Routes>
+        {team.map((member) => (
+          <Route path = {`/${member.name}`} element = {<Placeholder />}/>
+        ))}
+      </Routes>
     </div>
   );
 }
