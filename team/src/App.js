5b5a216616f709a2c6f96102022a5314eca390f9
Willis Li <li.willis95@gmail.com>
2022-05-08 17:14:16 -0500

    Added bare styling for homepage

--- team/src/App.js
+++ team/src/App.js
@@ -1,31 +1,32 @@
 import "./index.css";
 import { Routes, Route, NavLink } from "react-router-dom";
 import team from "./data/team.json";
-import MemberCard from "./Components/MemberCard";
+import logo from "./assets/trans-logo.png";
+import MemberCard from "./components/MemberCard";
 
 function App() {
   return (
-    <div className="App">
+    <div className = "min-h-screen bg-slate-100 flex items-center justify-center">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <header className="" exact path={"/"}>
-              <h1 className="">Ctrl Alt Delete</h1>
-              <div className="flex flex-row">
-                {team.map((member) => (
-                  <NavLink className="bg-slate-600 p-2" to={`/${member.id}`}>
-                    {member.name}
-                  </NavLink>
-                ))}
+        <Route exact path = "/" element = {
+            <header className = "flex flex-row justify-center items-center w-full">
+              <img className = "w-1/2 animate-slideLeft" src = {logo} alt = "teamLogo" />
+              <div className = "flex flex-col">
+                <h1 className = "text-6xl text-slate-500 font-bold animate-slideUp select-none my-4">Ctrl Alt Elite</h1>
+                <div className="flex flex-wrap">
+                  {team.map((member, index) => (
+                    <NavLink className = "bg-slate-600 p-2 rounded-lg w-1/3 text-center m-2 py-3 px-0 animate-slideUp" to = {`/${member.id}`}>
+                      {member.name}
+                    </NavLink>
+                  ))}
+                </div>
               </div>
             </header>
-          }
-        />
+        }/>
         {team.map((member) => (
           <Route
-            path={`/${member.id}`}
-            element={<MemberCard member={member} />}
+            path = {`/${member.id}`}
+            element = {<MemberCard member = {member} />}
           />
         ))}
       </Routes>
--- team/tailwind.config.js
+++ team/tailwind.config.js
@@ -7,17 +7,27 @@ module.exports = {
         'cards': '0 10px 20px -5px rgba(0, 0, 0, 0.3)',
       },
       animation: {
-        slideUp: "slideUp 350ms forwards",
+        slideUp: "slideUp 850ms forwards ease-out",
+        slideLeft: "slideLeft 1000ms forwards ease-in-out",
       },
       keyframes: {
         slideUp: {
           '0%': {
-            opacity: '0.4',
-            transform: 'translateY(50%);'
+            opacity: '0',
+            transform: 'translateY(100%);'
           },
           '100%': {
             opacity: '1',
             transform: 'translateY(0);'
+          }
+        },slideLeft: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(20%);'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0);'
           }
         },
       }
