e1f8b2a1ef9fac852617d85065319383f34aecfc
Daniel Kish <aox.soljah@gmail.com>
2022-05-09 10:56:45 -0500

    animation library

--- team/src/Components/MemberCard.js
+++ team/src/Components/MemberCard.js
@@ -4,28 +4,27 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import gsap from "gsap";
 
 export default function MemberCard(props) {
   const { member } = props;
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {member.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {member.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+    <Card elevation={21} sx={{ minWidth: "60%" }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h2" align="center" component="div">
+          {member.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {member.description}
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
